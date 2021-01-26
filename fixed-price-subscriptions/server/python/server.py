7a2b1f5e9287fa223ec98a95c97b50b5b9069726
CJ Avilla <cjavilla@stripe.com>
2021-01-26 11:32:23 -0800

    Import sinatra-cookies dependency

--- fixed-price-subscriptions/server/python/server.py
+++ fixed-price-subscriptions/server/python/server.py
@@ -14,10 +14,8 @@ load_dotenv(find_dotenv())
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 stripe.api_version = os.getenv('STRIPE_API_VERSION')
 
-static_dir = str(os.path.abspath(os.path.join(
-    __file__, "..", os.getenv("STATIC_DIR"))))
-app = Flask(__name__, static_folder=static_dir,
-            static_url_path="", template_folder=static_dir)
+static_dir = str(os.path.abspath(os.path.join(__file__, "..", os.getenv("STATIC_DIR"))))
+app = Flask(__name__, static_folder=static_dir, static_url_path="", template_folder=static_dir)
 
 
 @app.route('/', methods=['GET'])
