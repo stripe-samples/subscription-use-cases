2ef8f4cec18bf741342a05341461a3dfc065ffe6
seanzhang-stripe <92703819+seanzhang-stripe@users.noreply.github.com>
2024-09-13 14:14:02 +0800

    Fix warnings

--- usage-based-subscriptions/client/react/src/components/FlowContainer.jsx
+++ usage-based-subscriptions/client/react/src/components/FlowContainer.jsx
@@ -5,7 +5,6 @@ import StatusMessages from './StatusMessages'
 import './FlowContainer.css'
 import 'antd/dist/antd.min.css'
 
-const { Step } = Steps
 const FlowContainer = ({
   steps,
   messages,
@@ -31,29 +30,28 @@ const FlowContainer = ({
 
   return (
     <>
-      <Steps current={currentStep}>
-        {steps.map((step) => (
-          <Step key={step.title} title={step.title} />
-        ))}
-      </Steps>
+      <Steps current={currentStep} items={steps} />
       <div className="steps-content">{steps[currentStep].content}</div>
       <div className="steps-action">
+        <Button onClick={() => prev()} disabled={currentStep == 0}>
+          Previous
+        </Button>
         {currentStep < steps.length - 1 && (
-          <Button type="primary" onClick={() => next()}>
+          <Button
+            style={{ margin: '0 8px' }}
+            type="primary"
+            onClick={() => next()}
+          >
             Next
           </Button>
         )}
         {currentStep === steps.length - 1 && (
           <Button
             type="primary"
+            style={{ margin: '0 8px' }}
             onClick={() => message.success('Processing complete!')}
           >
             Done
-          </Button>
-        )}
-        {currentStep > 0 && (
-          <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
-            Previous
           </Button>
         )}
       </div>
